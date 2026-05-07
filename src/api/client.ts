60a1267e6ecd2cc943c6b69173cabada1899cf36
sarmad al-obaidi <sarmad.shatti@gmail.com>
2026-05-07 12:24:57 +0300

    push

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,13 +1,13 @@
-import ky from 'ky';
+import ky from "ky";
 
 const apiClient = ky.create({
   prefixUrl: "http://localhost:3000/api/v1",
   hooks: {
     beforeRequest: [
       (request) => {
-        const token = localStorage.getItem('authToken');
+        const token = localStorage.getItem("authToken");
         if (token) {
-          request.headers.set('Authorization', `Bearer ${token}`);
+          request.headers.set("Authorization", `Bearer ${token}`);
         }
       },
     ],
