ab00a0488605960a40a02d4e3e4e457cf41fbcbd
eleino <emilia.leino@proton.me>
2026-08-07 21:35:54 +0300

    added some responsiveness to image gallery

--- src/components/Images/ImageBox.tsx
+++ src/components/Images/ImageBox.tsx
@@ -2,7 +2,7 @@
 export const ImageBox = (props: { imageUrl: string, imageKey: string, type: string }) => {
   const { imageUrl, imageKey, type } = props;
   return (
-    <div className="bg-sidebar-grey rounded text-white h-50 w-80 p-2 flex flex-col items-center">
+    <div className="bg-sidebar-grey rounded text-white basis-auto h-50 w-90 p-2 flex flex-col items-center">
       <img src={imageUrl} alt={imageKey} className={`${type === "overlay" ? "w-auto" : "w-full"} " h-40 object-contain rounded"`} />
       <p className="p-1 self-start">Key: {imageKey}</p>
     </div>
